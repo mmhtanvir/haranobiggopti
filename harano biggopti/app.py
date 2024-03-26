03f72f3877261470b5ea2718a98fe6892f30449f
MH  Tanvir <82567711+mmhtanvir@users.noreply.github.com>
2024-03-26 06:39:03 +0600

    added multiple users, authentication, posting system

--- harano biggopti/app.py
+++ harano biggopti/app.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 import uuid
 import bcrypt
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+app.config['UPLOAD_FOLDER'] = 'static/images'
+
 app.secret_key = 'flash message'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/haranobiggopti'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# db starts
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
@@ -26,25 +27,108 @@ class User(db.Model):
     def check_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
     
-class post(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True)
-    number = db.Column(db.String(255), nullable=True)
-    password = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    category = db.Column(db.String(50))
+    description = db.Column(db.String(500))
+    status = db.Column(db.String(50))
+    security_question = db.Column(db.String(255))
+    security_question_answer = db.Column(db.String(255))
+    person_name = db.Column(db.String(255))
+    person_age = db.Column(db.String(255))
+    gender = db.Column(db.String(50))
+    animal = db.Column(db.String(50))
+    govt_paper_type = db.Column(db.String(50))
+    certificate_type = db.Column(db.String(50))
+    tags = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    created_by = db.Column(db.String(255))
+    timestamp = db.Column(db.TIMESTAMP, default=db.func.current_timestamp())
 
-    def __init__(self, name, email, number, password):
-        self.name = name
-        self.email = email
-        self.number = number
+    def __init__(self, title, category, description, status, security_question, security_question_answer, created_by, person_name=None, person_age=None, gender=None, animal=None, govt_paper_type=None, certificate_type=None, tags=None, image=None):
+        self.title = title
+        self.category = category
+        self.description = description
+        self.status = status
+        self.security_question = security_question
+        self.security_question_answer = security_question_answer
+        self.person_name = person_name
+        self.person_age = person_age
+        self.gender = gender
+        self.animal = animal
+        self.govt_paper_type = govt_paper_type
+        self.certificate_type = certificate_type
+        self.tags = tags
+        self.image = image
+        self.created_by = created_by
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    if 'name' in session:
+        name = session['name']
+    else:
+        name = None  
 
-@app.route("/create_posts")
+    posts = Post.query.all()
+
+    return render_template('index.html', posts=posts, name = name)
+
+@app.route("/create_posts", methods=['GET', 'POST'])
 def cp():
-    return render_template('cp.html')
+
+    if 'id' in session:
+        id = session['id']
+    else:
+        id = None  
+
+    if 'id' not in session:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        title = request.form['title']
+        category = request.form['category']
+        description = request.form['description']
+        status = request.form['status'] 
+        security_question = request.form['security_question']
+        security_question_answer = request.form['security_question_answer']
+        person_name = request.form.get('person_name')
+        person_age = request.form.get('person_age')
+        gender = request.form.get('gender')
+        animal = request.form.get('animal')
+        govt_paper_type = request.form.get('govt_paper_type')
+        certificate_type = request.form.get('certificate_type')
+        tags = request.form.get('tags')
+        img = str(uuid.uuid4())
+        image = request.files['image']
+        image.save('static/images/' + img + '.jpg')
+        image_filename = 'images/' + img + '.jpg'
+
+        if category == 'Person':
+            new_post = Post(title=title, category=category, description=description, status=status, security_question=security_question, security_question_answer=security_question_answer, person_name=person_name, person_age=person_age, gender=gender, tags=tags, image=image_filename, created_by=session['name'])
+        elif category == 'Pet':
+            new_post = Post(title=title, category=category, description=description, status=status, security_question=security_question, security_question_answer=security_question_answer, animal = animal, tags=tags, image=image_filename, created_by=session['name'])
+
+        elif category == 'govt_paper_type':
+            new_post = Post(title=title, category=category, description=description, status=status, security_question=security_question, security_question_answer=security_question_answer, govt_paper_type=govt_paper_type, tags=tags, image=image_filename, created_by=session['name'])
+
+        elif category == 'certificate_type':
+            new_post = Post(title=title, category=category, description=description, status=status, security_question=security_question, security_question_answer=security_question_answer, certificate_type=certificate_type, tags=tags, image=image_filename, created_by=session['name'])
+
+        new_post = Post(title=title, category=category, description=description, status=status, security_question=security_question, security_question_answer=security_question_answer, created_by=session['name'], person_name=person_name, person_age=person_age, gender=gender, animal=animal, govt_paper_type=govt_paper_type, certificate_type=certificate_type, tags=tags, image=image_filename)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('cp.html', id = id)
+
+@app.route('/category/<string:category>')
+def category(category):
+    
+    return render_template('category.html')
+
 
 # login/logout/signup
 
@@ -77,7 +161,9 @@ def signup():
     if 'number' in session:
         return redirect(url_for('index'))
 
+
     if request.method == 'POST':
+
         name = request.form['name']
         email = request.form['email']
         number = request.form['number']
@@ -99,19 +185,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-
-
-
-
-
-
-
-    id	
-    title	
-    description	
-    section	
-    category	
-    security	
-    image	
-    tags
